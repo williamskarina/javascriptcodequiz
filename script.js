b0bff9457f758696d9a3f30b4c4b064512dd0020
Karina Williams <coleman_karina@yahoo.com>
2021-07-22 16:12:13 -0700

    recommit

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
 var startButton = document.getElementById('start-button');
 var timeLeft = document.getElementById('timeLeft');
-var questionOne = document.getElementsByClassName('question-one');
-var questionTwo = document.getElementsByClassName('question-two');
-var questionThree = document.getElementsByClassName('question-three');
+var currentQuestionIndex = 0;
+startButton.addEventListener('click', startQuiz);
 
-startButton.addEventListener('click', secondsLeft);
+
 
 function secondsLeft() {
     var timeRemaining = 60;
@@ -20,8 +19,34 @@ function secondsLeft() {
     }, 1000);
 }
 
+function startQuiz() {
+    secondsLeft();
+    var questions = 
+    [
+      {
+        question: "What does JS stand for?",
+        choices: ["Javascript", "Just Studying", "John Stewart"],
+        answer: 1
+      },
 
+      {
+        question: "Is Javascript a programming language?",
+        choices: ["Yes", "No"],
+        answer: 1
+      },
 
-function firstQuestion() {
-    var numberOne = 'What does JS Stand for';
+      {
+        question: "What does NaN stand for?",
+        choices: ["No Actual Number", "Not a Number", "False"],
+        answer: 2
+      },
+
+      {
+        question: "What is the purpose of an array",
+        choices: ["to collect data", "to replace HTML", "to make a list of questions"],
+        answer: 1
+      }
+      
+      
+    ];
 }
