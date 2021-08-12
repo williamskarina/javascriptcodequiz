5fcd4ed772932b82653e6994a8d9dee22c49731f
Karina Williams <coleman_karina@yahoo.com>
2021-08-12 12:21:32 -0700

    finalized version

--- script.js
+++ script.js
@@ -1,54 +1,185 @@
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-var startButton = document.getElementById('start-button');
-var timeLeft = document.getElementById('timeLeft');
-var currentQuestionIndex = "index";
-startButton.addEventListener('click', startQuiz);
+const introEl = document.querySelector("#intro");
 
-function questionIndex() {
-    
-}
+const questionsEl = document.querySelector("#questions");
+let questionEl = document.querySelector("#question");
+let questionCount = 0;
+const yaynayEl = document.querySelector("#yaynay");
 
-function secondsLeft() {
-    var timeRemaining = 60;
+const finalEl = document.querySelector("#final");
+let initialsInput = document.querySelector("#initials");
 
-    var timeInterval = setInterval(function (){
-        if (timeRemaining > 0) {
-            timeLeft.textContent = timeRemaining + 'seconds remaining';
-            timeRemaining--;
+const highscoresEl = document.querySelector("#highscores");
+let scoreListEl = document.querySelector("#score-list");
+let scoreList = [];
+
+let timeEl = document.querySelector("p.time");
+let secondsLeft = 75;
+let scoreEl = document.querySelector("#score");
+
+const startBtn = document.querySelector("#start");
+const ansBtn = document.querySelectorAll("button.ansBtn")
+const ans1Btn = document.querySelector("#answer1");
+const ans2Btn = document.querySelector("#answer2");
+const ans3Btn = document.querySelector("#answer3");
+const ans4Btn = document.querySelector("#answer4");
+const submitScrBtn = document.querySelector("#submit-score");
+const goBackBtn = document.querySelector("#goback");
+const clearScrBtn = document.querySelector("#clearscores");
+const viewScrBtn = document.querySelector("#view-scores");
+
+const questions = [
+    {
+        question: "What does NaN stand for?",
+        answers: ["No Actual Number", "Not a Number", "False", "Nothing"],
+        correctAnswer: "1"
+    },
+    {
+        question: "What's not a keyword in JavaScript",
+        answers: ["this", "println", "boolean", "string"],
+        correctAnswer: "1"
+    },
+    {
+        question: "What is the purpose of an array",
+        answers: ["to collect data", "to replace HTML", "to make a list of questions", "to store objects"],
+        correctAnswer: "0"
+    },
+    {
+        question: "Is Javascript a programming language?",
+        answers: ["Yes", "No", "Not sure", "Don't know"],
+        correctAnswer: "0"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer: "3"
+    }
+];
+
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+            questionsEl.style.display = "none";
+            finalEl.style.display = "block";
+            scoreEl.textContent = secondsLeft;
         }
     }, 1000);
 }
 
 function startQuiz() {
-    secondsLeft();
-    var questions = 
-    [
-      {
-        question: "What does JS stand for?",
-        choices: ["Javascript", "Just Studying", "John Stewart"],
-        answer: 1
-      },
+    introEl.style.display = "none";
+    questionsEl.style.display = "block";
+    questionCount = 0;
 
-      {
-        question: "Is Javascript a programming language?",
-        choices: ["Yes", "No"],
-        answer: 1
-      },
-
-      {
-        question: "What does NaN stand for?",
-        choices: ["No Actual Number", "Not a Number", "False"],
-        answer: 2
-      },
-
-      {
-        question: "What is the purpose of an array",
-        choices: ["to collect data", "to replace HTML", "to make a list of questions"],
-        answer: 1
-      }
-      
-      
-    ];
+    setTime();
+    setQuestion(questionCount);
 }
+
+function setQuestion(id) {
+    if (id < questions.length) {
+        questionEl.textContent = questions[id].question;
+        ans1Btn.textContent = questions[id].answers[0];
+        ans2Btn.textContent = questions[id].answers[1];
+        ans3Btn.textContent = questions[id].answers[2];
+        ans4Btn.textContent = questions[id].answers[3];
+    }
+}
+
+function checkAnswer(event) {
+    event.preventDefault();
+
+    yaynayEl.style.display = "block";
+    let p = document.createElement("p");
+    yaynayEl.appendChild(p);
+
+    setTimeout(function () {
+        p.style.display = 'none';
+    }, 1000);
+
+    if (questions[questionCount].correctAnswer === event.target.value) {
+        p.textContent = "Correct!";
+    } else if (questions[questionCount].correctAnswer !== event.target.value) {
+        secondsLeft = secondsLeft - 10;
+        p.textContent = "Wrong!";
+    }
+
+    if (questionCount < questions.length) {
+        questionCount++;
+    }
+    setQuestion(questionCount);
+}
+
+function addScore(event) {
+    event.preventDefault();
+
+    finalEl.style.display = "none";
+    highscoresEl.style.display = "block";
+
+    let init = initialsInput.value.toUpperCase();
+    scoreList.push({ initials: init, score: secondsLeft });
+
+    scoreList = scoreList.sort((a, b) => {
+        if (a.score < b.score) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    
+    scoreListEl.innerHTML="";
+    for (let i = 0; i < scoreList.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scoreList[i].initials}: ${scoreList[i].score}`;
+        scoreListEl.append(li);
+    }
+
+    storeScores();
+    displayScores();
+}
+
+function storeScores() {
+    localStorage.setItem("scoreList", JSON.stringify(scoreList));
+}
+
+function displayScores() {
+    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
+
+    if (storedScoreList !== null) {
+        scoreList = storedScoreList;
+    }
+}
+
+function clearScores() {
+    localStorage.clear();
+    scoreListEl.innerHTML="";
+}
+
+startBtn.addEventListener("click", startQuiz);
+
+ansBtn.forEach(item => {
+    item.addEventListener('click', checkAnswer);
+});
+
+submitScrBtn.addEventListener("click", addScore);
+
+goBackBtn.addEventListener("click", function () {
+    highscoresEl.style.display = "none";
+    introEl.style.display = "block";
+    secondsLeft = 75;
+    timeEl.textContent = `Time:${secondsLeft}s`;
+});
+
+clearScrBtn.addEventListener("click", clearScores);
+
+viewScrBtn.addEventListener("click", function () {
+    if (highscoresEl.style.display === "none") {
+        highscoresEl.style.display = "block";
+    } else if (highscoresEl.style.display === "block") {
+        highscoresEl.style.display = "none";
+    } else {
+        return alert("No scores to show.");
+    }
+});
